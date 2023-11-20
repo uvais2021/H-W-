1d3c1008bd2f2937a169588dd6d70e3f6e535f19
Uvais A <138773333+uvais2021@users.noreply.github.com>
2023-11-20 15:52:14 +0530

    Update multipleTable.js

--- multipleTable.js
+++ multipleTable.js
@@ -2,7 +2,7 @@ function multi() {
     let num = parseInt(document.getElementById("multiNum").value);
     let result = document.getElementById("resultForMulti");
     let  u;
-    for (i = num; i <= 1000; i++) {
+    for (i = num; i <= 20; i++) {
        u = num * i;
      document.write(`${i} X ${num}=${u}<br>`);
     }
